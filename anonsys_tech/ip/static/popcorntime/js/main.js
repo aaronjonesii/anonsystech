52faa6bb43ae3736c14c5efdfd70adfbce8e29ce
Anonymous One <jonesaa5@msu.edu>
2018-09-27 01:11:43 -0400

    Attempt to animate shape of image by hovering

---
+++ anonsys_tech/ip/static/popcorntime/js/main.js
@@ -0,0 +1,11 @@
+
+
+// document.getElementById('showr').style.display='inline'
+
+// document.getElementById('showr').style.display='none'
+
+
+// $('#mov_img').hover(
+//     function () { $(this).addClass('rounded') },
+//     function () { $(this).removeClass('rounded-circle') }
+// )
