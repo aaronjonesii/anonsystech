2ff4c6294793206c5e8d65369f624d3e550e074f
root <jonesaa5@msu.edu>
2018-11-21 18:18:32 +0000

    Made filename dynamic for startup script

--- anonsys_tech/startServer.sh
+++ anonsys_tech/startServer.sh
@@ -1,1 +1,2 @@
-nohup python3 anonsys_tech/manage.py runserver 93.188.164.182:22299 >> 11142018.log &
+FILENAME=$(date +"%m%d%Y.log")
+nohup python3 anonsys_tech/manage.py runserver 93.188.164.182:22299 >> $FILENAME &
