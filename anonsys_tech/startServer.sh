e86d09649ca7e4a2ee216bfd71d374d7510bcd30
aaronjonesii <jonesaa5@msu.edu>
2018-12-28 10:38:11 -0500

    Update startServer.sh

--- anonsys_tech/startServer.sh
+++ anonsys_tech/startServer.sh
@@ -1,2 +1,2 @@
 FILENAME=$(date +"%m%d%Y.log")
-nohup python3 anonsys_tech/manage.py runserver 93.188.164.182:22299 >> $FILENAME &
+nohup python3 manage.py runserver 93.188.164.182:22299 >> ../$FILENAME &
